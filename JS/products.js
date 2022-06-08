b09130c7987dcd8039ea853028721a3beeb416fc
DarkMattz <febryangrady@gmail.com>
2022-06-08 19:01:50 +0700

    making category selection on products extensible

--- JS/products.js
+++ JS/products.js
@@ -1,41 +1,39 @@
 var SELECTION_STORAGE_KEY = 'SELECTION_STORAGE'
 
+let hideCategory = ['hair', 'face', 'body']
+let showCategory = new Array() 
+
 jQuery(() => {
+    $('.product-lists').css('visibility', 'visible')
     handleSelection(localStorage.getItem(SELECTION_STORAGE_KEY))
 })
 
+function categoryButtonClicked(category){
+    localStorage.setItem(SELECTION_STORAGE_KEY, category)
+    handleSelection(category)
+}
+
 function handleSelection(selection){
-    switch(selection){
-        case "hair":
-            hairBtnClicked()
-            break
-        case "face":
-            faceBtnClicked()
-            break
-        case "body":
-            bodyBtnClicked()
-            break
-        default:
-            bodyBtnClicked()
-            break
-    }
+    changeArray(selection)
+    manageDisplay()
+    resetArray()
 }
 
-function hairBtnClicked(){
-    console.log("hello")
-    $(".hair-products").removeClass("unsee");
-    $(".body-products").addClass("unsee");
-    $(".face-products").addClass("unsee");
+function changeArray(selection){
+    hideCategory.splice(hideCategory.indexOf(selection), 1)
+    showCategory.push(selection)
 }
 
-function faceBtnClicked(){
-    $(".face-products").removeClass("unsee");
-    $(".body-products").addClass("unsee");
-    $(".hair-products").addClass("unsee");
+function manageDisplay(){
+    hideCategory.forEach(category => {
+        $("." + category + "-products").addClass("unsee")
+    })
+    showCategory.forEach(category => {
+        $("." + category + "-products").removeClass("unsee")
+    })
 }
 
-function bodyBtnClicked(){
-    $(".body-products").removeClass("unsee");
-    $(".hair-products").addClass("unsee");
-    $(".face-products").addClass("unsee");
+function resetArray(){
+    hideCategory.push(showCategory[0])
+    showCategory.pop()
 }
